9fa50e795b042354d1b68b40257ae2df4a26d583
Artur Duarte <arturduartemoraes@gmail.com>
2023-12-18 18:22:31 -0300

    ♻️ refactor: indent code and change background

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
+'use strict';
+
 document.body.addEventListener('keyup', (event) => {
-    playSound(event.code.toLowerCase());
+	playSound(event.code.toLowerCase());
 });
 
 document.querySelector('.composer button').addEventListener('click', () => {
-    let song = document.querySelector('#input').value;
+	let song = document.querySelector('#input').value;
 
-    if (song !== '') {
-        let songArray = song.split('');
-        playComposition(songArray);
-    }
+	if (song !== '') {
+		let songArray = song.split('');
+		playComposition(songArray);
+	}
 });
 
 function playSound(sound) {
-    let audioElement = document.querySelector(`#s_${sound}`);
-    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+	let audioElement = document.querySelector(`#s_${sound}`);
+	let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
-    if (audioElement) {
-        audioElement.currentTime = 0;
-        audioElement.play();
-    }
+	if (audioElement) {
+		audioElement.currentTime = 0;
+		audioElement.play();
+	}
 
-    if (keyElement) {
-        keyElement.classList.add('active');
+	if (keyElement) {
+		keyElement.classList.add('active');
 
-        setTimeout(() => {
-            keyElement.classList.remove('active');
-        }, 300);
-    }
+		setTimeout(() => {
+			keyElement.classList.remove('active');
+		}, 300);
+	}
 }
 
 function playComposition(songArray) {
-    let wait = 0;
+	let wait = 0;
 
-    for (let songItem of songArray) {
-        setTimeout(() => {
-            playSound(`key${songItem}`);
-        }, wait);
+	for (let songItem of songArray) {
+		setTimeout(() => {
+			playSound(`key${songItem}`);
+		}, wait);
 
-        wait += 250;
-    }
+		wait += 250;
+	}
 }
